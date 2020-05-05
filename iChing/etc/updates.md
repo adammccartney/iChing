500473673672349c314f9798f3a0f220ffdc7d0e
adammccartney <adam@mur.at>
2020-05-05 11:21:52 +0200

    Added package structure including __init__ files, setup, manifest etc.

---
+++ iChing/etc/updates.md
@@ -0,0 +1,7 @@
+# Updates & Previous Versions
+-----------------------------
+
+v0.1 
+See git tag for date
+Initial commit, the program runs from a single script
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+# setup.py
+from distutils.core import setup 
+
+setup(name='iChing',
+        version='0.1',
+        author='Adam McCartney',
+        author_email='adam@mur.at',
+        url='https://admccartney.mur.at',
+        packages=['iChing', 'iChing.etc'],
+        )
