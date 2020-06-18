e520e812c56e1fe8bc7dd8167873b69bc70e7ecd
adammccartney <adam@mur.at>
2020-06-18 19:15:06 +0200

    hard reset, merged updated setup and travis config

--- setup.py
+++ setup.py
@@ -17,14 +17,15 @@ keywords = [
         "probability",
         ]
 
-setup(name='iChing',
-        version='0.1',
-        author='Adam McCartney',
-        author_email='adam@mur.at',
-        install_requires=install_requires,
-        keywords=", ".join(keywords),
-        name="iChing",
-        packages=['iChing'],
-        platforms="Any",
-        url="https://github.com/adamccartney/iChing"
+if __name__ == "__main__":
+    setuptools.setup(
+            version='0.1',
+            author='Adam McCartney',
+            author_email='adam@mur.at',
+            install_requires=install_requires,
+            keywords=", ".join(keywords),
+            name="iching",
+            packages=['iChing'],
+            platforms="Any",
+            url="https://github.com/adamccartney/iChing"
         )
